9fdfa8da3c561e1be14d3da437eb79e892970453
LazyTechwork <lazytechwork@gmail.com>
2020-12-11 15:11:40 +0300

    Fixed app crash

--- app/src/main/java/com/example/yard/application_services/MapsService.java
+++ app/src/main/java/com/example/yard/application_services/MapsService.java
@@ -1,8 +1,7 @@
 package com.example.yard.application_services;
 
+import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,11 +11,11 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.yard.R;
-import com.example.yard.Register;
 import com.example.yard.data.DataObject;
 import com.example.yard.data.Poll;
 import com.example.yard.utils.JSONInteractor;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -26,43 +25,46 @@ public class MapsService extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_maps_service);
-        Button mButton = findViewById(R.id.nextButton);
+        JSONInteractor jsonInteractor = new JSONInteractor(this, "data.json");
+        Activity activity = this;
+        try {
+            DataObject data = jsonInteractor.readJSON(DataObject.class);
+            setContentView(data.getMypolls().size() > 0 ? R.layout.activity_maps_service_filled : R.layout.activity_maps_service);
 
-        Context context = this;
+            if (data.getMypolls().size() == 0) {
+                Button mButton = findViewById(R.id.nextButton);
+                mButton.setOnClickListener(v -> {
+                    //CREATE ALERT
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MapsService.this);
+                    View view = getLayoutInflater().inflate(R.layout.alert_create_statement, null);
+                    builder.setView(view);
 
-        mButton.setOnClickListener(v -> {
-            //CREATE ALERT
-            AlertDialog.Builder builder = new AlertDialog.Builder(MapsService.this);
-            View view = getLayoutInflater().inflate(R.layout.alert_create_statement, null);
-            builder.setView(view);
+                    EditText formTitle = view.findViewById(R.id.title);
+                    EditText formText = view.findViewById(R.id.message);
+                    EditText formAddress = view.findViewById(R.id.address);
 
-            EditText formTitle = view.findViewById(R.id.title);
-            EditText formText = view.findViewById(R.id.message);
-            EditText formAddress = view.findViewById(R.id.address);
+                    builder.setPositiveButton(android.R.string.yes, (dialog, which) -> {
+                        try {
+                            ArrayList<Poll> polls = data.getPolls();
+                            int newId = polls.get(polls.size() - 1).getId() + 1;
+                            polls.add(new Poll(newId, 1, 0, formTitle.getText().toString(), formText.getText().toString(), formAddress.getText().toString(), "https://newtambov.ru/storage/taisia/2018/05/DSC02076.jpg"));
+                            data.setPolls(polls);
+                            ArrayList<Integer> my_polls = data.getMypolls();
+                            my_polls.add(newId);
+                            data.setMypolls(my_polls);
+                            jsonInteractor.writeJSON(data);
 
-            builder.setPositiveButton(android.R.string.yes, (dialog, which) -> {
-                JSONInteractor jsonInteractor = new JSONInteractor(context, "data.json");
-                try {
-                    DataObject data = jsonInteractor.readJSON(DataObject.class);
-                    ArrayList<Poll> polls = data.getPolls();
-                    int newId = polls.get(polls.size() - 1).getId() + 1;
-                    polls.add(new Poll(newId, 1, 0, formTitle.getText().toString(), formText.getText().toString(), formAddress.getText().toString(), "https://newtambov.ru/storage/taisia/2018/05/DSC02076.jpg"));
-                    data.setPolls(polls);
-                    ArrayList<Integer> my_polls = data.getMypolls();
-                    my_polls.add(newId);
-                    data.setMypolls(my_polls);
-                    jsonInteractor.writeJSON(data);
+                            activity.recreate();
+                        } catch (IOException e) {
+                            Log.e("Maps Service", "Error occurred while reading/writing data", e);
+                        }
+                    });
 
-                    Register.setDefaults("polls_created", "created", MapsService.this);
-                    setContentView(R.layout.activity_maps_service_filled);
-
-                } catch (IOException e) {
-                    Log.e("Maps Service", "Error occurred while reading/writing data", e);
-                }
-            });
-
-            builder.setNegativeButton(android.R.string.no, null).show();
-        });
+                    builder.setNegativeButton(android.R.string.no, null).show();
+                });
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("Maps Service", "Error occurred while reading/writing data", e);
+        }
     }
 }
