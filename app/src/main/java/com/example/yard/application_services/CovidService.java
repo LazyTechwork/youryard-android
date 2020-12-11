9662a170a941a18af9a0d18e8b66e9659307d852
LazyTechwork <lazytechwork@gmail.com>
2020-12-11 20:34:23 +0300

    Writing from alert to JSON

--- app/src/main/java/com/example/yard/application_services/CovidService.java
+++ app/src/main/java/com/example/yard/application_services/CovidService.java
@@ -1,7 +1,6 @@
 package com.example.yard.application_services;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,11 +21,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.yard.R;
-import com.example.yard.Register;
 import com.example.yard.adapters.CovidsAdapter;
-import com.example.yard.adapters.PollsAdapter;
 import com.example.yard.data.Covid;
 import com.example.yard.data.DataObject;
+import com.example.yard.data.Money;
 import com.example.yard.data.Poll;
 import com.example.yard.utils.JSONInteractor;
 import com.google.firebase.auth.FirebaseAuth;
@@ -61,43 +59,35 @@ public class CovidService extends AppCompatActivity {
         userID = mAuth.getCurrentUser().getUid();
 
         DocumentReference documentReference = fStore.collection("users").document(userID);
-        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                region_index = Integer.valueOf(value.getString("index"));
+        documentReference.addSnapshotListener((value, error) -> {
+            region_index = Integer.valueOf(value.getString("index"));
 
-                RequestQueue requestQueue = Volley.newRequestQueue(CovidService.this);
-                String url = "https://api.apify.com/v2/key-value-stores/1brJ0NLbQaJKPTWMO/records/LATEST?disableRedirect=true";
+            RequestQueue requestQueue = Volley.newRequestQueue(CovidService.this);
+            String url = "https://api.apify.com/v2/key-value-stores/1brJ0NLbQaJKPTWMO/records/LATEST?disableRedirect=true";
 
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            final String[] items = new String[85];
-                            final JSONArray array = response.getJSONArray("infectedByRegion");
-                            JSONObject jsonObject = array.getJSONObject(region_index);
-                            TextView mInfected = findViewById(R.id.textView_infected_number);
-                            TextView mDeceased = findViewById(R.id.textView_deceased_number);
-                            TextView mRecovered = findViewById(R.id.textView_recovered_number);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        final String[] items = new String[85];
+                        final JSONArray array = response.getJSONArray("infectedByRegion");
+                        JSONObject jsonObject = array.getJSONObject(region_index);
+                        TextView mInfected = findViewById(R.id.textView_infected_number);
+                        TextView mDeceased = findViewById(R.id.textView_deceased_number);
+                        TextView mRecovered = findViewById(R.id.textView_recovered_number);
 
-                            mInfected.setText(jsonObject.getString("infected"));
-                            mDeceased.setText(jsonObject.getString("deceased"));
-                            mRecovered.setText(jsonObject.getString("recovered"));
+                        mInfected.setText(jsonObject.getString("infected"));
+                        mDeceased.setText(jsonObject.getString("deceased"));
+                        mRecovered.setText(jsonObject.getString("recovered"));
 
-                        } catch (JSONException e) {
-                            Toast.makeText(CovidService.this, "Something really REALLY wrong", Toast.LENGTH_SHORT).show();
-                        }
+                    } catch (JSONException e) {
+                        Toast.makeText(CovidService.this, "Something really REALLY wrong", Toast.LENGTH_SHORT).show();
                     }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(CovidService.this, "Something wrong. Try again", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                }
+            }, error1 -> Toast.makeText(CovidService.this, "Something wrong. Try again", Toast.LENGTH_SHORT).show());
 
-                requestQueue.add(jsonObjectRequest);
+            requestQueue.add(jsonObjectRequest);
 
-            }
         });
 
 
@@ -118,24 +108,31 @@ public class CovidService extends AppCompatActivity {
 
         Button mButton;
         mButton = findViewById(R.id.button);
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                android.app.AlertDialog.Builder builder = new AlertDialog.Builder(CovidService.this);
-                View view = getLayoutInflater().inflate(R.layout.alert_create_statement_covid, null);
-                builder.setView(view);
+        mButton.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(CovidService.this);
+            View view = getLayoutInflater().inflate(R.layout.alert_create_statement_covid, null);
+            builder.setView(view);
 
-                EditText formTitle = view.findViewById(R.id.title);
-                EditText formText = view.findViewById(R.id.message);
-                EditText formAddress = view.findViewById(R.id.address);
-                EditText formPerson = view.findViewById(R.id.person);
+            EditText formTitle = view.findViewById(R.id.title);
+            EditText formText = view.findViewById(R.id.message);
+            EditText formAddress = view.findViewById(R.id.address);
+            EditText formPerson = view.findViewById(R.id.person);
 
-                builder.setPositiveButton(android.R.string.yes, (dialog, which) -> {
-                    //write here
-                });
+            builder.setPositiveButton(android.R.string.yes, (dialog, which) -> {
+                try {
+                    JSONInteractor jsonInteractor = new JSONInteractor(this, "data.json");
+                    DataObject data = jsonInteractor.readJSON(DataObject.class);
+                    ArrayList<Money> money = data.getMoney();
+                    int newId = money.get(money.size() - 1).getId() + 1;
+                    money.add(new Money(newId, 0, 100, formTitle.getText().toString(), formText.getText().toString(), formAddress.getText().toString(), "0", false));
+                    data.setMoney(money);
+                    jsonInteractor.writeJSON(data);
+                } catch (IOException e) {
+                    Log.e("COVID Service", "Error occurred while reading/writing data", e);
+                }
+            });
 
-                builder.setNegativeButton(android.R.string.no, null).show();
-            }
+            builder.setNegativeButton(android.R.string.no, null).show();
         });
 
     }
